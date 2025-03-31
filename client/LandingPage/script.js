8b5d111964e36c84124d5a47181aba4f63bad7d0
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-03-31 18:42:21 +0530

    added responsiveness to the navbar

---
+++ client/LandingPage/script.js
@@ -0,0 +1,16 @@
+function updateLoginButtonText() {
+    const loginButton = document.querySelector(".logIN-btn");
+    const button = document.querySelector(".signUP-btn");
+    const mediaQuery = window.matchMedia("(max-width: 900px)");
+
+    if(mediaQuery.matches) {
+        button.textContent = "Get started";
+        loginButton.style.display = "none";
+    }else{
+        button.textContent = "Sign up";
+        loginButton.style.display = "inline-block";
+    }
+}
+
+updateLoginButtonText();
+window.addEventListener("resize", updateLoginButtonText);
