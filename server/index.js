656bbef9efd59afefecb4334cc75d5e64fd5a4d9
Rouvik Maji <majirouvik@gmail.com>
2025-04-01 17:54:49 +0530

    Added clerk based authentication, click on the login and signup button to access
    Page hiding, and authenticated pages will be coming soon...
    Files changed:
    - client/LandingPage/index.html
    - Accounts/signin.html
    - Accounts/signup.html
    - index.js (node server authentication)

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const clerk = require("@clerk/express");
 
+// configure the environment variables
 dotenv.config();
 process.env.PORT = process.env.PORT || '8080';
 
+// the main app instance
 const app = express();
 
+// authentication -----------------
+app.use(clerk.clerkMiddleware());
+
+// base entry point of server
 app.use('/public', express.static('client'));
 
+app.use('/private', clerk.requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL, signUpUrl: process.env.CLERK_SIGN_UP_URL }), (req, res) => {
+    console.log("This is a protected route");
+});
+
+// main redirect
 app.get('/', (req, res) => {
     res.redirect('/public/LandingPage');
 });
 
+// http listen on PORT
 app.listen(+process.env.PORT, () => {
     console.log(`Server live at http://localhost:${process.env.PORT}`);
 });
