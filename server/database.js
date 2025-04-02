090d033b85a8d00b50d878627df1ab2f4332cca0
Rouvik Maji <majirouvik@gmail.com>
2025-04-02 22:30:01 +0530

    Added content for user database
    Added endpoints for user database
    - /private/api/incrank/:value/:set
    - /private/api/incstreak/:value/:set
    - /private/api/inccontest/:value/:set

---
+++ server/database.js
@@ -0,0 +1,148 @@
+const clerk = require("@clerk/express");
+const mongoose = require("mongoose");
+
+class MongooseConnect {
+    static isConnected = false;
+
+    static async connect(URI, force = false) {
+        if (force) {
+            MongooseConnect.isConnected = false;
+        }
+
+        if (!MongooseConnect.isConnected) {
+            await mongoose.connect(URI);
+            MongooseConnect.isConnected = true;
+            console.log("[MongooseConnect INFO] Successfully connected to MongoDB!");
+        }
+    }
+}
+
+class UserDBHandler {
+    static userSchema = new mongoose.Schema({
+        userID: String,
+        name: String,
+        rank: { type: Number, default: 0 },
+        contests_count: { type: Number, default: 0 },
+        streak_count: { type: Number, default: 0 },
+        admin: { type: Boolean, default: false }
+    });
+
+    static Users = mongoose.model("Users", UserDBHandler.userSchema);
+
+    async middleware_userAuth(req, res, next) {
+        if (!req.session.accountedFor) {
+            req.session.accountedFor = true;
+
+            try {
+                const userDbStore = await UserDBHandler.Users.findOne({
+                    userID: req.auth.userId
+                });
+
+                if (userDbStore === null) {
+                    const userData = await clerk.clerkClient.users.getUser(req.auth.userId);
+                    const userStore = new UserDBHandler.Users({
+                        userID: req.auth.userId,
+                        name: userData.username,
+                        rank: 0,
+                        contests_count: 0,
+                        streak_count: 0,
+                        admin: false
+                    });
+
+                    await userStore.save();
+                    console.log('No user');
+                }
+            } catch (error) {
+                console.log("[UserDBHandler Error] userAuthMiddleWare failed to authenticate user", error);
+            }
+        }
+
+        next();
+    }
+
+    /**
+     * Requires the req object to contain the param[optional] set to make sure the rank is set and not incremented
+     * @param {Request} req The request object
+     * @param {Response} res The response object
+     */
+    async endpoint_incrementUserRank(req, res) {
+        if (req.params.value) {
+            try {
+                this.incrementUserRank(req.auth.userId, req.params.value, req.params.set === "true");
+            } catch (err) {
+                res.json({ error: err.toString() });
+                return;
+            }
+        }
+
+        res.json({ message: 'ok' });
+    }
+
+    async incrementUserRank(_userId, value, set = false) {
+        if (set) {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $set: { rank: value } });
+        }
+        else {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $inc: { rank: value } });
+        }
+    }
+
+    /**
+     * Requires the req object to contain the param[optional] set to make sure the rank is set and not incremented
+     * @param {Request} req The request object
+     * @param {Response} res The response object
+     */
+    async endpoint_incrementStreakCount(req, res) {
+        if (req.params.value) {
+            try {
+                this.incrementStreakCount(req.auth.userId, req.params.value, req.params.set === "true");
+            } catch (err) {
+                res.json({ error: err.toString() });
+                return;
+            }
+        }
+
+        res.json({ message: 'ok' });
+    }
+
+    async incrementStreakCount(_userId, value, set = false) {
+        if (set) {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $set: { streak_count: value } });
+        }
+        else {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $inc: { streak_count: value } });
+        }
+    }
+
+    /**
+     * Requires the req object to contain the param[optional] set to make sure the rank is set and not incremented
+     * @param {Request} req The request object
+     * @param {Response} res The response object
+     */
+    async endpoint_incrementContestsCount(req, res) {
+        if (req.params.value) {
+            try {
+                this.incrementContestsCount(req.auth.userId, req.params.value, req.params.set === "true");
+            } catch (err) {
+                res.json({ error: err.toString() });
+                return;
+            }
+        }
+
+        res.json({ message: 'ok' });
+    }
+
+    async incrementContestsCount(_userId, value, set = false) {
+        if (set) {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $set: { contests_count: value } });
+        }
+        else {
+            await UserDBHandler.Users.updateOne({ userID: _userId }, { $inc: { contests_count: value } });
+        }
+    }
+}
+
+module.exports = {
+    MongooseConnect,
+    UserDBHandler
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const clerk = require("@clerk/express");
+const session = require("cookie-session");
+const crypto = require("node:crypto");
+
+const { MongooseConnect, UserDBHandler } = require("./database.js");
 
 // configure the environment variables
 dotenv.config();
@@ -9,13 +13,32 @@ process.env.PORT = process.env.PORT || '8080';
 // the main app instance
 const app = express();
 
+// session handler ----------------
+app.use(session({
+    secret: crypto.randomBytes(32),
+    maxAge: 24 * 60 * 60 * 1000     // 24 hour expiry
+}));
+
+// user database handler -------------
+MongooseConnect.connect(process.env.MONGO_DB_URL);
+const uDBHandler = new UserDBHandler();
+
 // authentication -----------------
 app.use(clerk.clerkMiddleware());
 
 // base entry point of server
-
 app.use('/public', express.static('client/public'));
-app.use('/private', clerk.requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL, signUpUrl: process.env.CLERK_SIGN_UP_URL }), express.static('client/private'));
+
+
+app.use('/private', clerk.requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL, signUpUrl: process.env.CLERK_SIGN_UP_URL }),
+                    uDBHandler.middleware_userAuth.bind(uDBHandler),
+                    express.static('client/private'));
+
+
+// api endpoints -----------------
+app.get('/private/api/incrank/:value/:set', uDBHandler.endpoint_incrementUserRank.bind(uDBHandler));
+app.get('/private/api/incstreak/:value/:set', uDBHandler.endpoint_incrementStreakCount.bind(uDBHandler));
+app.get('/private/api/inccontest/:value/:set', uDBHandler.endpoint_incrementContestsCount.bind(uDBHandler));
 
 // main redirect
 app.get('/', (req, res) => {
