c2133065076603efe780c9c64b2bf5a716752f8c
PixelPioneer404 <rajbeersaha.me@gmail.com>
2025-04-07 22:36:08 +0530

    added back-to-top button

--- client/public/LandingPage/script.js
+++ client/public/LandingPage/script.js
@@ -3,10 +3,10 @@ function updateLoginButtonText() {
     const button = document.querySelector(".signUP-btn");
     const mediaQuery = window.matchMedia("(max-width: 900px)");
 
-    if(mediaQuery.matches) {
+    if (mediaQuery.matches) {
         button.textContent = "Get started";
         loginButton.style.display = "none";
-    }else{
+    } else {
         button.textContent = "Sign up";
         loginButton.style.display = "inline-block";
     }
@@ -19,3 +19,35 @@ function toggleFAQ(element) {
     const faq = element.parentElement;
     faq.classList.toggle("open");
 }
+
+document.querySelector('#FAQ-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetFAQ = document.getElementById('FAQ');
+    if(targetFAQ) {
+        targetFAQ.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+});
+
+
+let backToTopBtn = document.querySelector(".back-to-top");
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+    const nearBottom = window.innerHeight + currentScrollY >= document.body.offsetHeight - 20;
+
+    if ((currentScrollY < lastScrollY && currentScrollY > 100) || nearBottom) {
+        backToTopBtn.classList.add('show');
+    } else {
+        backToTopBtn.classList.remove('show');
+    }
+    lastScrollY = currentScrollY;
+});
+backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
